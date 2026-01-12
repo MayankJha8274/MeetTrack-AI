d66c752d391f36b6bec1d026fa136324c4b5eaa4
Mayank <mayankjha8274@gmail.com>
2026-01-12 13:45:40 +0530

    Deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,10 @@
 const server =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+  import.meta?.env?.VITE_BACKEND_URL ||
+  process.env.REACT_APP_BACKEND_URL ||
+  "https://meettrack-ai.onrender.com";
 
 export default server;
+
 
 // frontend on vercel .
 // backend on render .
